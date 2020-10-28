da20e51a696f61d0b32784c05027c43c3c27d24e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-28 15:32:29 -0700

    updates envirionments for request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
 
   # config.web_socket_server_url = "wss://sezzle-calculator-backend.herokuapp.com/cable" 
-  # config.action_cable.allowed_request_origins = ['*', '*']
-
+  
   config.action_cable.url = "wss://sezzle-calculator-backend.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = [ 'https://sezzle-calculator-matthew-sedlacek.netlify.app', /http:\/\/https://sezzle-calculator-matthew-sedlacek.netlify.app.*/ ]
+  config.action_cable.allowed_request_origins = ['*', '*']
+  # config.action_cable.allowed_request_origins = [ 'https://sezzle-calculator-matthew-sedlacek.netlify.app', /http:\/\/https://sezzle-calculator-matthew-sedlacek.netlify.app.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
