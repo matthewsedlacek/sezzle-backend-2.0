99d266b94f19af369d7d4a3b22f3608ed66c4ca6
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-11-09 16:46:00 -0800

    changes custom created_at time in application.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def created_at
-    attributes['created_at'].strftime("%Y-%m-%d %H:%M %Z")
-  end
+  # def created_at
+  #   attributes['created_at'].strftime("%Y-%m-%d %H:%M %Z")
+  # end
 
 end
