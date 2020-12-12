47ff0932fe4bb805448e8a1f69928ec52f92530f
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-12-11 20:40:37 -0800

    removes methods in conversations channel and updates stream to messages

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,7 +1,8 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    conversation = Conversation.find(params[:conversation])
-    stream_for conversation
+    # conversation = Conversation.find(params[:conversation])
+    # stream_for conversation
+          stream_from "messages_channel"
   end
 
 end
